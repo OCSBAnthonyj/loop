8e0b52d0d355ecec6e4faf3127571d67e46ecec1
OCSBAnthonyj <113910453+OCSBAnthonyj@users.noreply.github.com>
2022-10-04 09:14:03 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,56 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    for (let index = 0; index < 4; index++) {
+        for (let index = 0; index < 4; index++) {
+            player.change(LedSpriteProperty.Y, 1)
+            basic.pause(100)
+        }
+        for (let index = 0; index < 4; index++) {
+            player.change(LedSpriteProperty.X, 1)
+            basic.pause(100)
+        }
+        for (let index = 0; index < 4; index++) {
+            player.change(LedSpriteProperty.Y, -1)
+            basic.pause(100)
+        }
+        for (let index = 0; index < 4; index++) {
+            player.change(LedSpriteProperty.X, -1)
+            basic.pause(100)
+        }
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    for (let index = 0; index < 4; index++) {
+        for (let index = 0; index < 4; index++) {
+            player.change(LedSpriteProperty.X, 1)
+            basic.pause(100)
+        }
+        for (let index = 0; index < 4; index++) {
+            player.change(LedSpriteProperty.Y, 1)
+            basic.pause(100)
+        }
+        for (let index = 0; index < 4; index++) {
+            player.change(LedSpriteProperty.X, -1)
+            basic.pause(100)
+        }
+        for (let index = 0; index < 4; index++) {
+            player.change(LedSpriteProperty.Y, -1)
+            basic.pause(100)
+        }
+    }
+})
+let player: game.LedSprite = null
+player = game.createSprite(0, 0)
